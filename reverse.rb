1ac48969d5b90156e24f612480f78e81bb2876b6
wavesirawich <sirawich.t@datawow.io>
2024-08-21 23:34:24 +0700

    reimplement reverse

--- reverse.rb
+++ reverse.rb
@@ -1,6 +1,12 @@
 def reverse(word)
-  puts word.reverse
+  new_reversed_char = []
+  raise ArgumentError, "Word cannot be nil" if word.nil?
+  (word.length - 1).downto(0) do |i|
+    new_reversed_char << word[i]
+  end
+  return new_reversed_char.join('')
 end
+
 
 reverse('hello')
 reverse('world')
