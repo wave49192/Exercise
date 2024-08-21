f46cd41b18f4f5c6797882322723304fdda20ef6
wavesirawich <sirawich.t@datawow.io>
2024-08-21 15:13:00 +0700

    fix bug

--- raindrop.rb
+++ raindrop.rb
@@ -1,18 +1,23 @@
 def raindrop(num)
   if num % 3 == 0
-    puts "pling"
-  elsif num % 5 == 0
-    puts "plang"
-  elsif num % 7 == 0
-    puts "plong"
-  else
+    print "pling"
+  end
+  if num % 5 == 0
+    print "plang"
+  end
+  if num % 7 == 0
+    print "plong"
+  end
+  unless  num % 3 == 0 or num % 5 == 0 or num % 7 == 0
     puts num
   end
+  puts ""
 end
 
 raindrop(6)
 raindrop(5)
 raindrop(7)
 raindrop(23)
+raindrop(30)
 
 # https://exercism.org/tracks/ruby/exercises/raindrops
