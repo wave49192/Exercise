f1e84dfbaab5fefc5eec6a6eca98a129ce247996
wavesirawich <sirawich.t@datawow.io>
2024-08-21 14:51:53 +0700

    q1

--- raindrop.rb
+++ raindrop.rb
@@ -1,1 +1,18 @@
+def raindrop(num)
+  if num % 3 == 0
+    puts "pling"
+  elsif num % 5 == 0
+    puts "plang"
+  elsif num % 7 == 0
+    puts "plong"
+  else
+    puts num
+  end
+end
+
+raindrop(6)
+raindrop(5)
+raindrop(7)
+raindrop(23)
+
 # https://exercism.org/tracks/ruby/exercises/raindrops
