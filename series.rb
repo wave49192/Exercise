a4d02b84de5dca4900fa3b37bdb745f2e91298a4
wavesirawich <sirawich.t@datawow.io>
2024-08-21 23:13:46 +0700

    reimplement series function

--- series.rb
+++ series.rb
@@ -1,10 +1,21 @@
 def series(nums, digit = 3)
-  nums = nums.to_a
-  nums.each_cons(digit) do |arr|
-    puts arr.join("")
+  raise ArgumentError, "Number of digits must be greater than 0" unless digit > 0
+  nums = nums.to_s
+  raise ArgumentError, "Digit is greater than the length of the number" if digit > nums.length
+
+  result = []
+  nums.each_char.each_cons(digit) do |arr|
+    result << arr.join("")
   end
+  result
 end
 
-series([1, 2, 3, 4, 5, 6], 5)
+series(123456, 6)
+#123456
+
+
+series(2342, 3)
+# 234
+# 342
 
 # https://exercism.org/tracks/ruby/exercises/series
