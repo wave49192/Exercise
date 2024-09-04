7e959287bc8a118eb434c77f18dc186408fa0685
wavesirawich <sirawich.t@datawow.io>
2024-09-04 11:40:36 +0700

    fix: fix spec

--- spec/series_spec.rb
+++ spec/series_spec.rb
@@ -1,17 +1,26 @@
-require './series.rb'
+RSpec.describe '#series' do
+  context 'valid digit' do
+    it 'returns full number' do
+      expect(series(123_456, 6)).to eq(['123456'])
+    end
 
-RSpec.describe "series" do
-  it 'should return array of correct values' do
-    expect(series(123456, 6)).to eq(["123456"])
-    expect(series(2342, 3)).to eq(["234", '342'])
+    it 'returns subseries' do
+      expect(series(2342, 3)).to eq(%w[234 342])
+    end
+
+    it 'handles multiple groups' do
+      expect(series(987_654_321, 4)).to eq(%w[9876 8765 7654 6543 5432 4321])
+    end
   end
 
-  it 'should raise error if digit is greater than the length of the number' do
-    expect { series(123456, 7) }.to raise_error(ArgumentError, "Digit is greater than the length of the number")
-  end
+  context 'invalid digit' do
+    it 'errors if too large' do
+      expect { series(123_456, 7) }.to raise_error(ArgumentError, 'Digit is greater than the length of the number')
+    end
 
-  it 'should raise error if digit less than or equal to 0' do
-    expect { series(123456, 0) }.to raise_error(ArgumentError, "Number of digits must be greater than 0")
-    expect { series(2342, -3) }.to raise_error(ArgumentError, "Number of digits must be greater than 0")
+    it 'errors if non-positive' do
+      expect { series(123_456, 0) }.to raise_error(ArgumentError, 'Number of digits must be greater than 0')
+      expect { series(2342, -3) }.to raise_error(ArgumentError, 'Number of digits must be greater than 0')
+    end
   end
 end
